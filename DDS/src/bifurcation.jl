5bf3b2240971ff7f54f0237479931808cd75310b
Jonas Koziorek <jonas.koziorek@gmail.com>
2023-12-20 14:16:15 +0100

    added multithreading, doesn't work well

--- DDS/src/bifurcation.jl
+++ DDS/src/bifurcation.jl
@@ -11,28 +11,52 @@ function pick_index(data, index)
 end
 
 function bifurcation_data(func, x0, params, param_index, param_range, total_n, sampling_n, index_x=1; left_p=nothing, right_p=nothing)
+    plotting_data = Vector{Tuple{Float64, Float64}}(undef, 0)
     if !isnothing(left_p) && !isnothing(right_p)
-        param_range = filter(x -> x < left_p || x > right_p, collect(param_range))
+        param_range = filter(x->x<left_p || x>right_p, collect(param_range))
     end
-    seqsize = sampling_n + 1
-    plotting_data = Vector{Tuple{Float64,Float64}}(undef, length(param_range)*seqsize)
-    for (i, param_value) in enumerate(param_range)
-        j = (i-1)*seqsize+1
+    for  param_value in param_range
         params[param_index] = param_value
         raw_data = iterate(func, x0, params, total_n)
         data = sample(raw_data, sampling_n)
         data = pick_index(data, index_x)
-        plotting_data[j:j+seqsize-1] = bifurcation_plot_point(param_value, data)
+        append!(plotting_data, bifurcation_plot_point(param_value, data))
     end
     return plotting_data
 end
 
+# function bifurcation_data(func, x0, params, param_index, param_range, total_n, sampling_n, index_x=1; left_p=nothing, right_p=nothing)
+#     if !isnothing(left_p) && !isnothing(right_p)
+#         param_range = filter(x -> x < left_p || x > right_p, collect(param_range))
+#     end
+#     plotting_data = Vector{Tuple{Float64,Float64}}(undef, 0)
+#     chunks = Iterators.partition(param_range, length(param_range) ÷ Threads.nthreads())
+#     tasks = map(chunks) do chunk
+#         Threads.@spawn trajectory_sample(func, copy(x0), copy(params), copy(param_index), chunk, copy(total_n), copy(sampling_n), copy(index_x))
+#     end
+#     thread_results = fetch.(tasks)
+#     for i in thread_results
+#         append!(plotting_data, i)
+#     end
+#     return plotting_data
+# end
+# function trajectory_sample(func, x0, params, param_index, param_range, total_n, sampling_n, index_x=1)
+#     thread_container = []
+#     for param_value in param_range
+#         params[param_index] = param_value
+#         raw_data = iterate(func, x0, params, total_n)
+#         data = sample(raw_data, sampling_n)
+#         data = pick_index(data, index_x)
+#         append!(thread_container, bifurcation_plot_point(param_value, data))
+#     end
+#     return thread_container
+# end
 
 function bifurcation_diagram(func, x0, params, param_index, param_range, total_n, sampling_n, index_x=1;  resolution = (800, 500), param_name = "parameter", kwargs...)
-    println("-----------")
-    @time plotting_data = bifurcation_data(func, x0, params, param_index, param_range, total_n, sampling_n, index_x)
+    println("\n-----------\n")
+    plotting_data = bifurcation_data(func, x0, params, param_index, param_range, total_n, sampling_n, index_x)
     fig = Figure(resolution=resolution)
-    @time bifurcation_diagram_axis(fig[1,1], plotting_data, param_name; kwargs...)
+    bifurcation_diagram_axis(fig[1,1], plotting_data, param_name; kwargs...)
     return fig
 end
 
