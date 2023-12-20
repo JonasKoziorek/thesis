eb2e386f6c2f898d6aef8dc961482fb6eb2e47b2
Jonas Koziorek <jonas.koziorek@gmail.com>
2023-12-20 17:42:36 +0100

    optimizing and refactoring bif diagrams

--- DDS/src/bifurcation.jl
+++ DDS/src/bifurcation.jl
@@ -1,56 +1,54 @@
-function sample(data, sampling_n::Int)
-    return data[end-sampling_n:end]
+function sample!(sampled_data::Vector, data::Vector, sampling_n::Int)
+    j = 1
+    for i in length(data)-sampling_n:length(data)
+        sampled_data[j] = data[i]
+        j+=1
+    end
 end
 
-function bifurcation_plot_point(param_value, data)
-    collect(zip([param_value for j=1:length(data)], data))
+function bifurcation_plot_point!(plot_points::Vector{Tuple{Float64, Float64}}, param_value::Float64, data::Vector)
+    for i in 1:length(data)
+        plot_points[i] = (param_value, data[i])
+    end
 end
 
-function pick_index(data, index)
-    return [i[index] for i in data]
+function pick_index!(data::Vector, index::Int)
+    for i in 1:length(data)
+        data[i] = data[i][index]
+    end
+end
+
+function prepare_data(total_n, sampling_n, param_range, x0)
+    seqlen = sampling_n + 1
+    plotting_data = Vector{Tuple{Float64, Float64}}(undef, seqlen*length(param_range))
+    raw_data = Vector{typeof(x0)}(undef, total_n+1)
+    sampled_data = Vector{typeof(x0)}(undef, sampling_n+1)
+    plot_points = Vector{Tuple{Float64, Float64}}(undef, sampling_n+1)
+    return plotting_data, raw_data, sampled_data, plot_points, seqlen
+end
+
+function process_param_value!(func, x0, params, param_index, param_value, total_n, sampling_n, index_x, raw_data, sampled_data, plot_points)
+    params[param_index] = param_value
+    iterate!(func, x0, params, total_n, raw_data)
+    pick_index!(raw_data, index_x)
+    sample!(sampled_data, raw_data, sampling_n)
+    bifurcation_plot_point!(plot_points, param_value, sampled_data)
 end
 
 function bifurcation_data(func, x0, params, param_index, param_range, total_n, sampling_n, index_x=1; left_p=nothing, right_p=nothing)
-    plotting_data = Vector{Tuple{Float64, Float64}}(undef, 0)
     if !isnothing(left_p) && !isnothing(right_p)
         param_range = filter(x->x<left_p || x>right_p, collect(param_range))
     end
-    for  param_value in param_range
-        params[param_index] = param_value
-        raw_data = iterate(func, x0, params, total_n)
-        data = sample(raw_data, sampling_n)
-        data = pick_index(data, index_x)
-        append!(plotting_data, bifurcation_plot_point(param_value, data))
+    plotting_data, raw_data, sampled_data, plot_points, seqlen = prepare_data(total_n, sampling_n, param_range, x0)
+    i = 0
+    for param_value in param_range
+        j = i*seqlen + 1
+        process_param_value!(func, x0, params, param_index, param_value, total_n, sampling_n, index_x, raw_data, sampled_data, plot_points)
+        @views plotting_data[j:j+seqlen-1] = plot_points
+        i += 1
     end
     return plotting_data
 end
-
-# function bifurcation_data(func, x0, params, param_index, param_range, total_n, sampling_n, index_x=1; left_p=nothing, right_p=nothing)
-#     if !isnothing(left_p) && !isnothing(right_p)
-#         param_range = filter(x -> x < left_p || x > right_p, collect(param_range))
-#     end
-#     plotting_data = Vector{Tuple{Float64,Float64}}(undef, 0)
-#     chunks = Iterators.partition(param_range, length(param_range) ÷ Threads.nthreads())
-#     tasks = map(chunks) do chunk
-#         Threads.@spawn trajectory_sample(func, copy(x0), copy(params), copy(param_index), chunk, copy(total_n), copy(sampling_n), copy(index_x))
-#     end
-#     thread_results = fetch.(tasks)
-#     for i in thread_results
-#         append!(plotting_data, i)
-#     end
-#     return plotting_data
-# end
-# function trajectory_sample(func, x0, params, param_index, param_range, total_n, sampling_n, index_x=1)
-#     thread_container = []
-#     for param_value in param_range
-#         params[param_index] = param_value
-#         raw_data = iterate(func, x0, params, total_n)
-#         data = sample(raw_data, sampling_n)
-#         data = pick_index(data, index_x)
-#         append!(thread_container, bifurcation_plot_point(param_value, data))
-#     end
-#     return thread_container
-# end
 
 function bifurcation_diagram(func, x0, params, param_index, param_range, total_n, sampling_n, index_x=1;  resolution = (800, 500), param_name = "parameter", kwargs...)
     println("\n-----------\n")
