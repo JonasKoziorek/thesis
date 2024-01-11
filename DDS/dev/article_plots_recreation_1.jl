9e6e11371c21c5248422477e5abe4469a3f7fd26
Jonas Koziorek <jonas.koziorek@gmail.com>
2024-01-11 09:46:27 +0100

    moving files around

--- DDS/dev/article_plots_recreation_1.jl
+++ DDS/dev/article_plots_recreation_1.jl
@@ -1,3 +1,5 @@
+# reconstructing plots from article Intermittency Reinjection in the Logistic Map from Elaskar and Del Rio
+
 using DDS
 using CairoMakie: scatter!, Figure, display, scatter
 using Plots: scatterhist, plot!
@@ -66,6 +68,7 @@ begin
     ))
 end
 
+# # seems to be in infinite loop
 # begin
 #     data = Float64[]
 #     for _ in 1:N
@@ -102,50 +105,4 @@ begin
     ax.ylabelsize = 25
     ax.xlabelsize = 25
     display(fig)
-end
-
-using CairoMakie
-begin
-    # num = 3
-    # data = [(0, 0), (0, 1), (1, 0)]
-    # scatter(data, color = 1:num, colormap=:heat, markersize = 50)
-
-    n = 100;
-    x = LinRange(0, 2*pi, n);
-    y = sin.(x);
-    z = x
-
-    # set color axis limits which by default is automatically equal to the extrema of the color values
-    colorrange = [0, 2*pi];
-    cmap = :heat
-    
-    # make colored scatter plot
-    fig = Figure()
-    scatter(fig[1, 1], x, y; color=z, colormap = cmap, markersize=10, strokewidth=0, colorrange = colorrange)
-    scatter!(fig[1, 1], [(5.5, 0),(5.5, 0.5),(5.5, 0.6)]; color=[5.5, 5.5, 5.5], colormap = cmap, markersize=10, strokewidth=0, colorrange = colorrange)
-
-    # add colorbar 
-    Colorbar(fig[1, 2], colorrange = colorrange, colormap = cmap, label = "colored data")
-    fig
-end
-
-begin
-    total_n = 1000
-    last_n = 50
-    p_range = LinRange(3.82835, 3.8285, 10000)
-    figure = Figure()
-    left_p = 1+sqrt(8)-2.5*1e-5
-    right_p = 1+sqrt(8)-1e-7
-    ax = DDS.colorize_bif_diag!(figure[1,1], logistic, 0.5, param, 1, p_range, total_n, last_n, left_p, right_p;param_name="")
-    ax.xlabel = ""
-    ax.ylabel = ""
-
-    x0 = 0.15992881844625645
-    map_ = DDS.nth_composition(DDS.logistic, 3)
-    nth_logistic = (x, p) -> map_(x+x0, p) - x0
-    f(p) = nth_logistic(0, [p])^(-0.5)
-    colorrange = [f(left_p), f(right_p)];
-    cmap = :rainbow
-    Colorbar(figure[1, 2], colorrange = colorrange, colormap = cmap, label="average laminar phase length")
-    display(figure)
 end
