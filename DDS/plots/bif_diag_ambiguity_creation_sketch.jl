20ceeb6f988ae99cffea725a59320b9fe1633fcc
Jonas Koziorek <jonas.koziorek@gmail.com>
2024-03-24 17:55:43 +0100

    updated ambiguity plot

--- DDS/plots/bif_diag_ambiguity_creation_sketch.jl
+++ DDS/plots/bif_diag_ambiguity_creation_sketch.jl
@@ -33,13 +33,16 @@ begin
             # title="title"
         )
     ax.aspect = 5
+    ax.xlabel = L"n"
+    ax.ylabel = L"x_{n}"
+    ax.ylabelsize = ax.xlabelsize = 30
 
     x0 = 0.5
     p = [4.47458285]
     custom_trajectory!(
         ax, pomeau_manneville, x0, p, total_n;
         markersize=5.0, 
-        linewidth=0.04,
+        linewidth=0.00,
         dot_color=RED,
         line_color=:black,
     )
@@ -49,7 +52,7 @@ begin
     custom_trajectory!(
         ax, pomeau_manneville, x0, p, total_n;
         markersize=5.0, 
-        linewidth=0.04,
+        linewidth=0.00,
         dot_color=BLUE,
         line_color=:black,
     )
@@ -66,36 +69,37 @@ begin
     img = readpng(DDS.FIGURES_DIRECTORY * "bif_diag_ambiguity_creation_sketch.png")
     w = img.width
     h = img.height
-    # scale(0.4, 0.4)
+    scalar = 0.5
+    scale(scalar, scalar)
     placeimage(img, Point(-w/2, -h/2), 1.0)
     sethue("black")
     setline(4)
-    height_ = 30
-    mid = 10
-    p1 = Point(-120+mid, height_)
+    height_ = 15
+    mid = 50
+    p1 = Point(-200+mid, height_)
     p2 = Point(mid, height_)
     Luxor.arrow(p1, p2,arrowheadlength=20)
     Luxor.arrow(p2, p1,arrowheadlength=20)
 
     p1 = Point(mid, height_)
-    p2 = Point(mid+170, height_)
+    p2 = Point(mid+280, height_)
     Luxor.arrow(p1, p2,arrowheadlength=20)
     Luxor.arrow(p2, p1,arrowheadlength=20)
 
-    height_ = 15
-    fontsize(26)
-    x = -95
+    height_ = 5
+    fontsize(46)
+    x = -120
     y = height_
     text(L"a", Point(x, y), halign=:center, valign=:baseline)
 
-    x = 30
+    x = 35
     y = height_
     text(L"b", Point(x, y), halign=:center, valign=:baseline)
 
-    x = 160
+    x = 300
     y = height_
     text(L"c", Point(x, y), halign=:center, valign=:baseline)
 # end 1000 250
-end 1000 250 file_path
+end 900 250 file_path
 
 end
